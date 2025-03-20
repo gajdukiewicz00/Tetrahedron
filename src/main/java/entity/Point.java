0a4bf5d25a92bcc29dec5d2bf64e7c62f330454f
Denis Gajdukiewicz <den.94dew@gmail.com>
2025-03-20 17:00:39 +0100

    first try

--- src/main/java/entity/Point.java
+++ src/main/java/entity/Point.java
@@ -1,4 +1,37 @@
 package entity;
 
 public class Point {
+    private double x;
+    private double y;
+    private double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getZ() { return z; }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) + Double.hashCode(y) + Double.hashCode(z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point point = (Point) obj;
+        return Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0 &&
+                Double.compare(point.z, z) == 0;
+    }
 }
--- src/main/java/util/FileReader.java
+++ src/main/java/util/FileReader.java
@@ -1,4 +1,41 @@
 package util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import entity.Point;
+import entity.Tetrahedron;
+
 public class FileReader {
+
+    public static List<Tetrahedron> readTetrahedrons(String fileName) {
+        List<Tetrahedron> tetrahedrons = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName));
+            for (String line : lines) {
+                String[] parts = line.split(" ");
+                if (parts.length == 12) {
+                    try {
+                        Point[] vertices = new Point[4];
+                        for (int i = 0; i < 4; i++) {
+                            double x = Double.parseDouble(parts[i * 3]);
+                            double y = Double.parseDouble(parts[i * 3 + 1]);
+                            double z = Double.parseDouble(parts[i * 3 + 2]);
+                            vertices[i] = new Point(x, y, z);
+                        }
+                        tetrahedrons.add(new Tetrahedron(vertices));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Invalid number format: " + line);
+                    }
+                } else {
+                    System.err.println("Invalid data length: " + line);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+        return tetrahedrons;
+    }
 }
